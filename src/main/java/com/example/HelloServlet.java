2003ef8081735635517d812cfd856df1d4016304
CFFA <87035436+retroandroid@users.noreply.github.com>
2026-02-06 22:21:17 +0200

    uploading the files

---
+++ src/main/java/com/example/HelloServlet.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("text/plain; charset=UTF-8");
+        resp.getWriter().println("Hello Christian Attieh");
+    }
+}
